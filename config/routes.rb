f1703b035a3af53f3cb2aaba748cad952ec75f40
Lawrence Chege <chegzlaw@gmail.com>
2018-02-12 00:33:19 +0300

    Installed Devise and Devise users Admin, Vendor and Couple

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :couples
+  devise_for :vendors
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#index'
